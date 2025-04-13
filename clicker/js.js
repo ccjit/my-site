1acdcb7d71ca54a6f97f89e0254bda229e3d23a2
ccjt <circjit@gmail.com>
2025-04-13 14:31:53 -0300

    Update js.js

--- clicker/js.js
+++ clicker/js.js
@@ -1,8 +1,38 @@
 if (localStorage.clicks == undefined) {
     localStorage.setItem('clicks', 0)
 }
+if (localStorage.clickers == undefined) {
+    let price = 10
+}
+let clicks = localStorage.clicks
 document.getElementById("timesClicked").innerHTML = localStorage.clicks;
 function addCount() {
-    localStorage.setItem('clicks', parseInt(localStorage.clicks) + 1);
+    if (clicks == localStorage.clicks) {
+        localStorage.setItem('clicks', parseInt(localStorage.clicks) + 1);
+    } else {
+        localStorage.setItem('clicks', 0);
+    }
+    let clicks = localStorage.clicks
     document.getElementById("timesClicked").innerHTML = localStorage.clicks;
-}  
+}
+function buyClicker() {
+    if (localStorage.clickers == undefined) {
+        localStorage.setItem('clickers', 0)
+    }
+    if (parseInt(localStorage.clicks) < price) {
+        document.getElementById("timesClicked").innerHTML = "You don't have enough clicks to buy that.";
+        setTimeout(function(){document.getElementById("timesClicked").innerHTML = localStorage.clicks},5000)
+    } else {
+        localStorage.setItem("clickers", parseInt(localStorage.clickers) + 1)
+        localStorage.setItem('clicks', parseInt(localStorage.clicks) - price)
+        document.getElementById("timesClicked").innerHTML = localStorage.clicks;
+        document.getElementById("clickerAmount").innerHTML = localStorage.clickers;
+        let price = price + (price / 2)
+    }
+}
+if (parseInt(localStorage.clickers) > 0) {
+    setInterval(function () {
+        localStorage.setItem('clicks', parseInt(localStorage.clicks) + 1); 
+        document.getElementById("timesClicked").innerHTML = localStorage.clicks;
+    }, 10000 / parseInt(localStorage.clickers))
+}
