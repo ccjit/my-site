e402f922f4adee964c356162809d0aefb6d0fb34
ccjt <circjit@gmail.com>
2025-04-12 21:31:06 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,8 @@
-// add javascript code here
+function randomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+if (localStorage.token == undefined) {
+    localStorage.setItem(randomString(24, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"))
+}
