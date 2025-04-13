3671b28f6e7738f21b6b066b93a84b8f234f759a
ccjt <circjit@gmail.com>
2025-04-12 22:26:29 -0300

    Update js.js

--- clicker/js.js
+++ clicker/js.js
@@ -2,7 +2,7 @@ if (localStorage.clicks == undefined) {
     localStorage.setItem('clicks', 0)
 }
 document.getElementById("timesClicked").innerHTML = localStorage.clicks;
-function btnClick() {
+function addCount() {
     localStorage.setItem('clicks', parseInt(localStorage.clicks) + 1);
     document.getElementById("timesClicked").innerHTML = localStorage.clicks;
 }  
