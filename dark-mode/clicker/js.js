8dda2afecd5c468d8a589273be6ec4b0125cf88b
ccjt <circjit@gmail.com>
2025-05-14 13:36:20 -0300

    Update js.js[

--- dark-mode/clicker/js.js
+++ dark-mode/clicker/js.js
@@ -6,6 +6,10 @@ if (localStorage.clickers == undefined || localStorage.clickers == "0") {
     localStorage.setItem('clickers', 0)
 }
 let autoClick
+if (localStorage.clickers !== 0) {
+     autoClick = setInterval(addCount(), 2000 / parseInt(localStorage.clickers))
+}
+let clicks = parseInt(localStorage.clicks)
 function addCount() {
     localStorage.setItem('clicks', parseInt(localStorage.clicks) + 1);
     document.getElementById("timesClicked").innerHTML = localStorage.clicks;
@@ -25,16 +29,22 @@ function buyClicker() {
         setTimeout(function(){document.getElementById("clickers").innerHTML = localStorage.clickers},5000)
     } else {
         localStorage.setItem("clickers", parseInt(localStorage.clickers) + 1)
-        document.getElementById("clickers").innerHTML = localStorage.clickers;
+        document.getElementById("clickers").innerHTML = localStorage.clickers;0
         localStorage.setItem('clicks', parseInt(localStorage.clicks) - parseInt(localStorage.clickerPrice))
         document.getElementById("timesClicked").innerHTML = localStorage.clicks;
-        localStorage.setItem('clickerPrice', parseInt(localStorage.clickerPrice) + (parseInt(localStorage.clickerPrice) / 2))
+        localStorage.setItem('clickerPrice', Math.round(parseInt(localStorage.clickerPrice) + (parseInt(localStorage.clickerPrice) / 2)))
         document.getElementById("price").innerHTML = "Clicker price: " + localStorage.clickerPrice;
         clicker()
     }
 }
 let clickclick = {
     reset: function(no, parameters) { localStorage.setItem('clicks', 0); localStorage.setItem('clickers', 0); localStorage.setItem('clickerPrice', 10); document.getElementById("clickers").innerHTML = localStorage.clickers; document.getElementById("timesClicked").innerHTML = localStorage.clicks; },
+    hardreset: function(reloadPageType) { localStorage.removeItem('clicks'); localStorage.removeItem('clickers'); localStorage.removeItem('clickerPrice'); if (typeof reloadPageType === undefined) { if (reloadPageType === 0) { window.location.reload(true); } else if (reloadPageType === 1) { window.location.reload(false); } else if (reloadPageType !== 0 && reloadPagetype !== 1) { console.error("That is not a valid value. Please specify a valid value. 0 = refresh and load new page from server, 1 = refresh from cache") } } else { window.location.reload(true) } },
     price: console.log("Clicker Price: " + localStorage.clickerPrice)
 }
-clicker()
+
+setTimeout(()=>{
+    if (localStorage.clickers !== 0) {
+        clicker()
+    }
+}, 1000)
