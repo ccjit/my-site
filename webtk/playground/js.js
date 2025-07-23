518987ad4f20d7bfac40efba9e9816defc729f61
ccjt <circjit@gmail.com>
2025-07-23 20:31:03 -0300

    Update js.js

--- webtk/playground/js.js
+++ webtk/playground/js.js
@@ -1,4 +1,4 @@
-const webtk = import("https://raw.githubusercontent.com/ccjit/wintk/refs/heads/main/webtk.js");console.log('%cLoaded MPPCT! uwu','color:orange; font-size:15px;');
+import * as webtk from 'https://raw.githubusercontent.com/ccjit/wintk/refs/heads/main/webtk.js';
 console.log('%cLoaded WebTK.','color:pink; font-size:20px;');
 console.log("The WebTK documentation page is yet to be made.")
 console.log(webtk)
